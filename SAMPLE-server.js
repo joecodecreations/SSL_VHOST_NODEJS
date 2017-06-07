ad2d8ee13f3229834af89a55aa5baa22c374553a
Joseph <joseph@codecreations.net>
2017-06-06 22:11:21 -0400

    updated to some es6

--- SAMPLE-server.js
+++ SAMPLE-server.js
@@ -1,43 +1,41 @@
-var connect = require('connect');
-var vhost = require('vhost');
-var express = require('express');
-var http = require('http');
-var https = require('https');
-var helmet = require('helmet');
-var fs = require('fs');
+const connect = require('connect'),
+  vhost = require('vhost'),
+  express = require('express'),
+  http = require('http'),
+  https = require('https'),
+  helmet = require('helmet'),
+  fs = require('fs');
 
 /*SSL PRIVATE AND PUBLIC KEYS */
-var pathToPrivateKey = "/some/path.pem";
-var pathToPublicKey = "/some/path.pem";
-var privateKey = fs.readFileSync(pathToPrivateKey, 'utf8');
-var certificate = fs.readFileSync(pathToPublicKey, 'utf8');
-
-var credentials = {
+const pathToPrivateKey = "/some/path.pem",
+  pathToPublicKey = "/some/path.pem",
+  privateKey = fs.readFileSync(pathToPrivateKey, 'utf8'),
+  certificate = fs.readFileSync(pathToPublicKey, 'utf8'),
+  credentials = {
     key: privateKey,
     cert: certificate
-};
+  };
 
-var main = express();
-/* Cron Jobs */
+let main = express();
 
 /* Site One */
-var siteOnePath = "./sites/siteOne/server"; //path to site one js file
-var siteOne = require(siteOnePath); //require that site file
+const siteOnePath = "./sites/siteOne/server"; //path to site one js file
+const siteOne = require(siteOnePath); //require that site file
 main.use(vhost('siteOneDomain.com', siteOne)); //when we see that domain use this path
 main.use(vhost('www.siteOneDomain.com', siteOne)); //also include www path
 
 /* Site Two */
-var siteTwoPath = "./sites/siteTwo/server";
-var siteTwo = require(siteTwoPath);
+const siteTwoPath = "./sites/siteTwo/server";
+const siteTwo = require(siteTwoPath);
 main.use(vhost('siteTwoDomain.com', siteTwo));
 main.use(vhost('www.siteTwoDomain.com', siteTwo));
 
 //...Duplicate and add as many sites here as you would like
 
-var httpsServer = https.createServer(credentials, main);
+let httpsServer = https.createServer(credentials, main);
 
 /* listen for connections on 80 and 433 for ssl */
 main.listen(80);
 main.listen(8080);
 httpsServer.listen(443);
-console.log('Server successfully started');
+console.log('Node server successfully started');
--- sites/siteOne/server.js
+++ sites/siteOne/server.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var urlStatus = require('url-status');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var helmet = require('helmet');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  urlStatus = require('url-status'),
+  fs = require('fs'),
+  http = require('http'),
+  https = require('https'),
+  helmet = require('helmet');
 
 
 /* initiate express server */
-var app = express();
+let app = express();
 /* Allow ejs templating */
 app.set('view engine', 'ejs');
 /* Allow body variables */
 app.use(bodyParser.urlencoded({
-    'extended': 'true'
+  'extended': 'true'
 }));
 /* Allow json */
 app.use(bodyParser.json());
@@ -21,13 +21,12 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public_html'));
 /* Stricter HTTPS */
 app.use(helmet({
-    hsts: true
+  hsts: true
 }));
 
-
 /* add in routes as normal */
-app.get('/', function (req, res) {
-    res.send("hello world this is site one");
+app.get('/', (req, res) => {
+  res.send("hello world this is site one");
 });
 
 //Export instead of creating server
